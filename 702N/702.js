90c512c0eeba5a47262b1aad6d22f31451cb74bd
KAR0218 <82988257+KAR0218@users.noreply.github.com>
2021-11-24 18:38:38 +0100

    Added GoldHEN V2.0b

--- 702N/702.js
+++ 702N/702.js
@@ -827,6 +827,10 @@ function toggle_payload(pld){
 		document.getElementById("progress").innerHTML="Loading GoldHenv1.0.. Please wait..";
 		PLfile = 'goldhen1.bin';
 		preloadScripts(['jb.js', 'loader.js']);
+	}else if(pld == "goldhen2b"){
+		document.getElementById("progress").innerHTML="Loading GoldHenv2.0b.. Please wait..";
+		PLfile = 'goldhen2b702.bin';
+		preloadScripts(['jb.js', 'loader.js']);
 	}else if(pld == "ftp"){
 		setTimeout(function(){document.getElementById("progress").innerHTML="FTP Loaded.. Access at port 1337.."; }, 7000);
 		PLfile = 'ftp.bin';
@@ -965,6 +969,7 @@ function done(){
 			'<br><br><a href="#" id="hen213b" class="button" onclick="toggle_payload(\'hen213b\'); return false" style="font-size:25px;text-align:center;text-shadow: 4px 4px 4px black;color:white">Hen213b</a>&nbsp;'+
 			'<a href="#" id="goldhen1" class="button" onclick="toggle_payload(\'goldhen1\'); return false" style="font-size:25px;text-align:center;text-shadow: 4px 4px 4px black;color:yellow">GoldHenV1.0</a>&nbsp;'+
 			'<a href="#" id="goldhen" class="button" onclick="toggle_payload(\'goldhen\'); return false" style="font-size:25px;text-align:center;text-shadow: 4px 4px 4px black;color:yellow">GoldHenV1.1</a>&nbsp;'+
+			'<a href="#" id="goldhen2b" class="button" onclick="toggle_payload(\'goldhen2b\'); return false" style="font-size:25px;text-align:center;text-shadow: 4px 4px 4px black;color:yellow">GoldHenV2b</a>&nbsp;'+
 			'<a href="#" id="binloader" class="button" onclick="toggle_payload(\'binloader\'); return false" style="font-size:25px;text-align:center;text-shadow: 4px 4px 4px black;color:white">Bin Loader</a>&nbsp;'+
 			'</td>'+
 			'</tr>'+
