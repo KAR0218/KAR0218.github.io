2cfe17de456f5e9c7f9be185b35090f8f62e9b77
KAR0218 <82988257+KAR0218@users.noreply.github.com>
2024-03-25 03:56:23 +0100

    Update Webkit to PSFree

--- restore/755L/int64.js
+++ restore/755L/int64.js
@@ -1,7 +1,127 @@
 // Taken from https://github.com/saelo/jscpwn/blob/master/int64.js
 //
 // Copyright (c) 2016 Samuel Groß
+function int64(low, hi) {
+    this.low = (low >>> 0);
+    this.hi = (hi >>> 0);
 
+    this.add32inplace = function (val) {
+        var new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
+        var new_hi = (this.hi >>> 0);
+
+        if (new_lo < this.low) {
+            new_hi++;
+        }
+
+        this.hi = new_hi;
+        this.low = new_lo;
+    }
+
+    this.add32 = function (val) {
+        var new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
+        var new_hi = (this.hi >>> 0);
+
+        if (new_lo < this.low) {
+            new_hi++;
+        }
+
+        return new int64(new_lo, new_hi);
+    }
+
+    this.sub32 = function (val) {
+        var new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
+        var new_hi = (this.hi >>> 0);
+
+        if (new_lo > (this.low) & 0xFFFFFFFF) {
+            new_hi--;
+        }
+
+        return new int64(new_lo, new_hi);
+    }
+
+    this.add64 = function(val) {
+        var new_lo = (((this.low >>> 0) + val.low) & 0xFFFFFFFF) >>> 0;
+        var new_hi = (this.hi >>> 0);
+
+        if (new_lo > (this.low) & 0xFFFFFFFF) {
+            new_hi++;
+        }
+        new_hi = (((new_hi >>> 0) + val.hi) & 0xFFFFFFFF) >>> 0;
+        return new int64(new_lo, new_hi);
+    }
+    this.sub64 = function(val) {
+        var new_lo = (((this.low >>> 0) - val.low) & 0xFFFFFFFF) >>> 0;
+        var new_hi = (this.hi >>> 0);
+
+        if (new_lo > (this.low) & 0xFFFFFFFF) {
+            new_hi--;
+        }
+        new_hi = (((new_hi >>> 0) - val.hi) & 0xFFFFFFFF) >>> 0;
+        return new int64(new_lo, new_hi);
+    }
+
+    this.sub32inplace = function (val) {
+        var new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
+        var new_hi = (this.hi >>> 0);
+
+        if (new_lo > (this.low) & 0xFFFFFFFF) {
+            new_hi--;
+        }
+
+        this.hi = new_hi;
+        this.low = new_lo;
+    }
+
+    this.and32 = function (val) {
+        var new_lo = this.low & val;
+        var new_hi = this.hi;
+        return new int64(new_lo, new_hi);
+    }
+
+    this.and64 = function (vallo, valhi) {
+        var new_lo = this.low & vallo;
+        var new_hi = this.hi & valhi;
+        return new int64(new_lo, new_hi);
+    }
+
+    this.toString = function (val) {
+        val = 16;
+        var lo_str = (this.low >>> 0).toString(val);
+        var hi_str = (this.hi >>> 0).toString(val);
+
+        if (this.hi == 0)
+            return lo_str;
+        else
+            lo_str = zeroFill(lo_str, 8)
+
+        return hi_str + lo_str;
+    }
+
+    this.toPacked = function () {
+        return {
+            hi: this.hi,
+            low: this.low
+        };
+    }
+
+    this.setPacked = function (pck) {
+        this.hi = pck.hi;
+        this.low = pck.low;
+        return this;
+    }
+
+    return this;
+}
+
+function zeroFill(number, width) {
+    width -= number.toString().length;
+
+    if (width > 0) {
+        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
+    }
+
+    return number + ""; // always return a string
+}
 function Int64(low, high) {
     var bytes = new Uint8Array(8);
 
